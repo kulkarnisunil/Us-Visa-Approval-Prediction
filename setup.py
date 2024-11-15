8ebab6a063259a1e3e3cc4ae2fe4f95a790635b9
kulkarnisunil <sunilkulkarni603@gmail.com>
2024-11-15 11:26:21 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sunil Kulkarni",
+    author_email="sunilkulkarni603@gmail.com",
+    packages=find_packages()
+)
